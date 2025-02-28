729bce236668a2a1ec8b0a12e0c64419f36737ef
Arsamerzoev Magomed <o22401211@or-iut-i207-f18.iut45.univ-orleans.fr>
2025-02-28 15:14:57 +0100

    implémentation du fichier testBox

---
+++ BoxTest.java
@@ -0,0 +1,8 @@
+import org.junit.*;
+import static org.junit.Assert.assertEquals;
+    public BoxTest class {
+        @Test
+        public void testBoxCreate() {
+            Box b = new Box();
+        }
+    };
